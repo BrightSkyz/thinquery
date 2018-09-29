5eae59cc9c463b4be939ac18128f89cdf31c9180
BrightSkyz <BrightSkyz@users.noreply.github.com>
2018-09-29 18:53:30 -0400

    Clean up code

--- underline.js
+++ underline.js
@@ -1,57 +1,57 @@
 const _element = Symbol();
 const _elements = Symbol();
-const _length = Symbol();
 
 function _(query) {
-	return new Underline(document.querySelectorAll(query));
+  return new Underline(document.querySelectorAll(query));
 }
 
-class Underline {  
-	constructor(elements) {
-		this[_elements] = [].slice.call(elements);
-  	this[_length] = this[_elements].length;
-	}
-  
-  toArray() {
-		return [].slice.call(this);
-	}
-  
-	get(number) {
-		if (number == null) {
-			return [].slice.call(this[_elements]);
-		}
-		if (number < 0) {
-    	return this[number + this[_elements]];
-    } else {
-    	return this[number];
+class Underline {
+  constructor(elements) {
+    this[_elements] = [];
+    for (var i in elements) {
+      this[_elements].push(new UnderlineElement(elements[i]));
     }
-	}
-  
+    this[_elements] = [].slice.call(elements);
+  }
+
+  toArray() {
+    return [].slice.call(this);
+  }
+
+  get(number) {
+    if (number == null) {
+      return this[_elements][0];
+    }
+    return this[_elements][number]
+  }
+
   each(callback) {
-  	for (var i in this[_elements]) {
-    	callback.call({element: new UnderlineElement(this[_elements][i])});
+    for (var i in this[_elements]) {
+      callback.call({
+        element: this[_elements][i]
+      });
     }
   }
 }
 
 class UnderlineElement {
-	constructor(element) {
+  constructor(element) {
     this[_element] = element;
   }
-  
+
   html(htmlString) {
-		if (htmlString == null) {
-			return this[_element].innerHTML;
-		} else {
-    	this[_element].innerHTML = htmlString;
+    if (htmlString == null) {
+      return this[_element].innerHTML;
+    } else {
+      this[_element].innerHTML = htmlString;
     }
-	}
-  
+  }
+
   text(textString) {
-		if (textString == null) {
-			return this[_element].innerText;
-		} else {
-    	this[_element].innerText = textString;
+    if (textString == null) {
+      return this[_element].innerText;
+    } else {
+      this[_element].innerText = textString;
     }
-	}
+  }
 }
